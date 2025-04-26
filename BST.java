7d57450a5a83c0a65ae253fc9e06257f4dbb7492
Haifasu <hss.1438@hotmail.com>
2025-04-26 15:15:56 +0300

    Update BST.java

--- BST.java
+++ BST.java
@@ -1,11 +1,10 @@
-
 public class BST<T> {
         public class BSTNode <T> {
             public String key;
             public T data;
             public BSTNode<T> left, right;
 
-            // Creates a new instance of BSTNode 
+            /** Creates a new instance of BSTNode */
             public BSTNode(String k, T val) {
                     key = k;
                     data = val;
@@ -234,5 +233,4 @@ public class BST<T> {
              if ( p.right !=null)
                  inorder ( p.right);
          }
-
 }
